77dc8c2544da2657d7a5509116795cb53eeafc85
sberezowskaya <143963198+sberezowskaya@users.noreply.github.com>
2025-10-27 15:21:11 +0300

    загрузка сгенерированного файла

---
+++ src/main/java/com/example/IGUMNOVA/IgumnovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.IGUMNOVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgumnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgumnovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IGUMNOVA/IgumnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IGUMNOVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgumnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
